d5f21601a1d247f86745a910f9de46728bc4106b
mihir <cmihirhere@gmail.com>
2015-11-22 11:35:51 +0530

    temp remove require for definitions

--- createmydemo.php
+++ createmydemo.php
@@ -1,20 +1,17 @@
 <?php
 define("_BOOKMAN_INIT",true);
 require_once("private/conn.php");
-require_once("definitions.php");
+//require_once("definitions.php");
 date_default_timezone_set("Asia/Calcutta");
 $today = new Date();
 if(isset($_POST)){
-$username = filter_var($_POST['username']);
-$password = MD5(filter_var($_POST['password']));
-$subdomain = filter_var($_POST['subdomain']);
-
-$insert = $db->query("INSERT INTO #_clients_subdomains VALUES ('','','$username','$password','$subdomain','$today','1')");
-if($insert){
-echo 'data inserted successfully!';
-
-
-}
+	$username = filter_var($_POST['username']);
+	$password = MD5(filter_var($_POST['password']));
+	$subdomain = filter_var($_POST['subdomain']);
+	$insert = $db->query("INSERT INTO #_clients_subdomains VALUES ('','','$username','$password','$subdomain','$today','1')");
+		if($insert){
+			echo 'data inserted successfully!';
+					}
 
 
 }
@@ -34,11 +31,7 @@ echo 'data inserted successfully!';
 		<input type="text" name="username" />
 		<input type="password" name="password" />
 		<input type="text" name="subdomain" />
-
 		<input type="submit" name="add_sub_domain" />
-
-
-
 	</form>
 
 
