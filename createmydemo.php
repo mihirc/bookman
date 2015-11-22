cf84513b21a94603e08504f10ca32d5667d6f8b7
mihir <cmihirhere@gmail.com>
2015-11-22 10:57:04 +0530

    create my demo file added

---
+++ createmydemo.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+
+?>
+
+
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>Create my Demo || BookMan</title>
+</head>
+<body>
+<div>
+	<form method="post" action="">
+		<input type="text" name="username" />
+		<input type="password" name="password" />
+		<input type="text" name="subdomain" />
+
+		<input type="submit" name="add_sub_domain" />
+
+
+
+	</form>
+
+
+
+</div>
+</body>
+</html>
