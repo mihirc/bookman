22145175880fd6a033d7fc59e5a30166034883e2
Girish Pawar <girish@thoughtfulviewfinder.in>
2016-06-02 13:32:55 +0530

    first commit aft long time

--- access/superman.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined("_BOOKMAN_INIT") or die("Direct permission to this file disallowed");
-$string='{"dbname":"superman","username":"8Nk6rPlCBe","password":"b\\\"0R+_PyRy","expirydate":"2015-12-10"}';
-
-?>
