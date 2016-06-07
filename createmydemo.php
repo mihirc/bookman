8e1a90f8f5567c22199b45c7cc24049bfbe68067
Girish Pawar <girish@thoughtfulviewfinder.in>
2016-06-07 13:22:14 +0530

    changes

--- createmydemo.php
+++ createmydemo.php
@@ -343,7 +343,7 @@ $sendgrid = new SendGrid('SG.le7G3fE4SVGlQir25cdPBQ.C-fAZfe7rNa7PihIXM48G9t0YEzQ
 $email = new SendGrid\Email();
 $email
     ->addTo($mailid)
-    ->setFrom('info@thoughtfulviewfinder.tk', 'Thoughtfulviewfinder Services')
+    ->setFrom('info@bookman.in', 'Thoughtfulviewfinder Services')
     ->setSubject('Welcome to Bookman')
     ->setText('Hello World!')
     ->addUniqueArg("foliage_message_id", "22")
